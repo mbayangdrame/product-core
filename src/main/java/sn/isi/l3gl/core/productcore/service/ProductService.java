5092125f80baed4d2a31a440877b4e4c8fcc069b
mbayangdrame <mb.drame3@isepdiamniadio.edusn>
2026-03-15 13:57:54 +0000

    update version to 0.1.0-SNAPSHOT for listProducts()

--- src/main/java/sn/isi/l3gl/core/productcore/service/ProductService.java
+++ src/main/java/sn/isi/l3gl/core/productcore/service/ProductService.java
@@ -1,5 +1,7 @@
 package sn.isi.l3gl.core.productcore.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import sn.isi.l3gl.core.productcore.repository.ProductRepository;
 import sn.isi.l3gl.core.productcore.entity.Product;
@@ -15,5 +17,10 @@ public class ProductService {
 
     public Product createProduct(Product product){
         return repository.save(product);
+    }
+
+
+    public List<Product> listProducts(){
+    return repository.findAll();
     }
 }
