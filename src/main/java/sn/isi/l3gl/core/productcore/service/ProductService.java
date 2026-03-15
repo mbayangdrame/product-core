9b381416af13b53ba7d0f4269770a8c4430209bc
mbayangdrame <mb.drame3@isepdiamniadio.edusn>
2026-03-15 13:26:07 +0000

    feature createProduct

---
+++ src/main/java/sn/isi/l3gl/core/productcore/service/ProductService.java
@@ -0,0 +1,19 @@
+package sn.isi.l3gl.core.productcore.service;
+
+import org.springframework.stereotype.Service;
+import sn.isi.l3gl.core.productcore.repository.ProductRepository;
+import sn.isi.l3gl.core.productcore.entity.Product;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository){
+        this.repository = repository;
+    }
+
+    public Product createProduct(Product product){
+        return repository.save(product);
+    }
+}
